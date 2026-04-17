e0351d640ada54448a57a26f26d6e68c4a15e8da
Muhammad Ahsan <43028548+ahsan-storm@users.noreply.github.com>
2026-04-18 01:18:31 +0200

    Pin Formula sha256 for v0.1.0

--- Formula/kp.rb
+++ Formula/kp.rb
@@ -2,7 +2,7 @@ class Kp < Formula
   desc "Kill TCP listeners on a port"
   homepage "https://github.com/ahsan-storm/homebrew-kp"
   url "https://github.com/ahsan-storm/homebrew-kp/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_AFTER_RELEASE"
+  sha256 "37ace94e220886eec2e1763604feb7c98eebe526eee5a03c93c41c3ca473949a"
   license "MIT"
 
   def install
